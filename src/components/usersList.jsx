fe33ee585dc3b30a57a4d45ea4e4d02da65f8a18
Artur Zhitkovich <artur.zhitkovich15041998@gmail.com>
2022-08-15 18:31:06 +0300

    Update usersList.jsx

--- src/components/usersList.jsx
+++ src/components/usersList.jsx
@@ -6,7 +6,7 @@ import React from "react";
 const UsersList = () => {
     const params = useParams();
     const { userId } = params;
-    return <>{userId ? <User id={userId} /> : <Users />}</>;
+    return <>{userId ? <User id={userId} /> : <Users />}</>; // Это две разные страниы, и их надо разделять через router 
 };
 
 export default UsersList;
